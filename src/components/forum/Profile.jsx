16b89a68aa03f247231221727db2d51c53530d60
Karmen <karbonkarmen@gmail.com>
2022-03-21 14:47:45 +0800

    new changes

--- src/components/forum/Profile.jsx
+++ src/components/forum/Profile.jsx
@@ -7,14 +7,14 @@ function Profile() {
     <div className="about">
       <div class="container">
         <div class="row align-items-center my-5">
-          <div class="col-lg-7">
+          <div class="col-lg-3">
             <img
               class="img-fluid rounded mb-4 mb-lg-0"
               src="http://placehold.it/900x400"
               alt=""
             />
           </div>
-          <div class="col-lg-5">
+          <div class="col-lg-6">
             <h1 class="font-weight-bold">Profile</h1>
             <h2 class="font-weight-light">@username</h2>
             <NavLink className="nav-link" to="/account">
@@ -54,10 +54,10 @@ function Profile() {
 const Button = styled.button`
   background-color: white;
   color: black;
-  font-size: 15px;
-  padding: 8px 150px;
+  font-size: 12px;
+  padding: 5px 350px;
   border-radius: 0px;
-  margin: 10px 10px
+  margin: 1px 100px
   cursor: pointer;
 `;
 
