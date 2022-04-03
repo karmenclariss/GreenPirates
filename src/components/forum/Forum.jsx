5f3ac40b4f39d42361fa905c695eec3dde3c3c05
yingxuanhehe <63840887+yingxuanhehe@users.noreply.github.com>
2022-04-03 20:04:53 +0800

    Update Forum.jsx

--- src/components/forum/Forum.jsx
+++ src/components/forum/Forum.jsx
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from "react"; //To handle change, need to track the state of the input
 import {Link} from "react-router-dom";
 import Pagination from './Pagination';
-
-
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 function Forum(){
 
     const [threads, setThreads] = useState([{
@@ -43,7 +43,10 @@ function Forum(){
         </div>
 
         <div class="col-lg-2">
-        <Link to="/createPost" className="btn btn-primary">Create Post</Link>
+        <NavLink className="nav-link" to="/createPost">
+            <Button>CREATE A POST</Button>
+        </NavLink>
+        {/* <Link to="/createPost" className="btn btn-primary">Create Post</Link> */}
         </div>
 
         <div class="container">
@@ -63,5 +66,16 @@ function Forum(){
     )
 
 }
-
+const Button = styled.button`
+  background-color: white;
+  padding: 8px 8px 8px 32px;
+  color: black;
+  font-size: 12px;
+  border-radius: 10px;
+  margin: 1px 100px
+  cursor: pointer;
+  background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABMAAAATCAYAAAByUDbMAAAABmJLR0QA/wD/AP+gvaeTAAAA7UlEQVQ4jb2UTQ6CQAyFv4gxEXfq0jvoxp97qZxJb+Fag3oBFihnYKESXdCJk8kUERNf0jTTljd9pQB/RgD0xTdCD4iAGCiAJ/CQ8xoI6xItgEwINLsC8zpE+QciYzkwq5KmdXRS4hdNclQhCfG+/MpHFivFieQTJX8wBG3xATB2yDPg5nQG0AFGVt1Eni9MYOjcdvS1bsGd4cBOBpR75M4qAXZSs5OzO7s7noVuOrO9IWhZZBtFUsfxLra+YOiRYOysxFOgq1zCnO++gKlGZDCj3OwqorQOkS15RbmQd95v7QAsq6R9ws//s0Z4AYPMj/ARKOC7AAAAAElFTkSuQmCC);
+  background-repeat: no-repeat;
+  background-position: 8px 8px;
+`;
 export default Forum;
